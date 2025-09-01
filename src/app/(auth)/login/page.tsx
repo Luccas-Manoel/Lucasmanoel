09b74c3b0481ff5464e02eec6c997255074b46ae
Luccas-Manoel <estudos.lucasmanoel@gmail.com>
2025-09-01 13:04:29 -0300

    Build feita

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ export default function login(){
     return(
         <body>
             <footer></footer>
-            <main><h1>Página de login</h1></main>
+            <main><h1>Página de login!</h1></main>
             <footer></footer>
         </body>
     )
